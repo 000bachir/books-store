ef325768cc6d4eab5b1ce195fc26c44590af67c3
000bachir <peakdev698@gmail.com>
2025-09-28 14:24:43 +0100

    api need some fixes

--- src/routes/best-seller/+page.server.ts
+++ src/routes/best-seller/+page.server.ts
@@ -1,23 +1,45 @@
 import type { PageServerLoad } from "./$types";
 import { NEW_YORK_TIMES_API_KEY } from "$env/static/private"
 import { error, json } from "@sveltejs/kit";
+import type { NytBook } from "$lib/types";
 export const load: PageServerLoad = async ({ params }) => {
-    try{
+    try {
         const apikey = NEW_YORK_TIMES_API_KEY;
-        if (!apikey){
+        if (!apikey) {
             console.error("error loading the api key from the .env file please check both files for more info");
             return;
         }
 
-        const request = await fetch('https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json?api-key='+apikey);
-        if(!request.ok){
+        const request = await fetch('https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json?api-key=' + apikey);
+        if (!request.ok) {
             console.error(`error couldn't make the api call properly the status of the error is : ${request.status} and the full error message should be : ${request.statusText}`)
             return
         }
-        const response = await request.json()
-        //console.log(JSON.stringify(response , null , 2))
-        console.log(response.results.books[0]);
-    }catch(error) {
+        const response = await request.json();
+        console.log(response)
+        if (!response.results.books || !Array.isArray(response.results.books)) {
+            console.log("error not a single items has been returned from the api call")
+            return {
+                books: []
+            }
+        }
+
+        let BestSellers = response.items.map((item: NytBook) => ({
+            author: item.author,
+            title: item.title,
+            descreption: item.description,
+            price: item.price,
+            thumbnail: item.book_image,
+            buyLinks: item.buy_links
+        }))
+
+        console.log(BestSellers)
+
+        return {
+            BestSellers: BestSellers
+        }
+
+    } catch (error) {
         console.error("ERROR something happened during the make of the api call : ", error)
     }
 }
--- src/routes/best-seller/+page.svelte
+++ src/routes/best-seller/+page.svelte
@@ -1,13 +1,69 @@
 <script lang="ts">
-    import { onMount } from "svelte";
-    import type { PageData } from './$types';
+	import { onMount } from 'svelte';
+	import type { PageData } from './$types';
+	let { data } = $props();
+	console.log(data.BestSellers);
 
 
-    let {data} = $props();
-    console.log(data)
-
+    let loading: boolean = $state(true)
 </script>
 
+<main class="relative flex h-auto w-full items-center justify-center overflow-hidden flex-col">
+	<h1 class="py-6 mb-8 text-center text-3xl font-bold sm:text-4xl">Best Sellers...👓</h1>
 
-<main class="">
+	<div
+		class="relative flex h-auto w-full flex-col items-center justify-center gap-12 overflow-hidden py-8"
+	>
+		<h2 class="text-2xl font-semibold text-red-600">Trigger Warning!!!</h2>
+		<p class="text-center text-lg text-balance">
+            Carefull choosing your next drug 😉
+		</p>
+	</div>
+
+    {#if loading}
+        
+    {/if}
+
+	<section class="relative mx-auto h-auto w-[95%] py-4">
+		<!--first card-->
+		<div
+			class="max-w-sm rounded-lg border border-gray-200 bg-white shadow-sm dark:border-gray-700 dark:bg-gray-800"
+		>
+			<a href="/">
+				<img class="rounded-t-lg" src="https://picsum.photos/600/400/?random" alt="" />
+			</a>
+			<div class="p-5">
+				<a href="/">
+					<h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+						Noteworthy technology acquisitions 2021
+					</h5>
+				</a>
+				<p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
+					Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse
+					chronological order.
+				</p>
+				<a
+					href="/"
+					class="inline-flex items-center rounded-lg bg-blue-700 px-3 py-2 text-center text-sm font-medium text-white hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 focus:outline-none dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+				>
+					Read more
+					<svg
+						class="ms-2 h-3.5 w-3.5 rtl:rotate-180"
+						aria-hidden="true"
+						xmlns="http://www.w3.org/2000/svg"
+						fill="none"
+						viewBox="0 0 14 10"
+					>
+						<path
+							stroke="currentColor"
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							stroke-width="2"
+							d="M1 5h12m0 0L9 1m4 4L9 9"
+						/>
+					</svg>
+				</a>
+			</div>
+		</div>
+	</section>
 </main>
