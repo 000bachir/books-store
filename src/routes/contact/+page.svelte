2b61360c2b3f4a37ee2e9485e776eb20732b80ec
000bachir <peakdev698@gmail.com>
2025-10-13 23:43:16 +0100

    done for now

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,87 +1,56 @@
 <script lang="ts">
-	import { Linkedin } from 'lucide-svelte';
-	import { Send } from 'lucide-svelte';
-
-	const Works = [
-		{ name: 'Github', href: 'https://github.com/000bachir', id: 'github' },
-		{
-			name: 'Linkedin',
-			href: 'https://www.linkedin.com/in/seridi-mohammed-98a70423b/',
-			id: 'linkedin'
-		}
-	];
-
-	const Freelances = [
-		{
-			name: 'Upwork',
-			href: 'https://www.upwork.com/freelancers/~01a03f0469a3b5fcc2',
-			id: 'Upwork'
-		},
-		{
-			name: 'Fiver',
-			href: 'https://fr.fiverr.com/users/bachir_24/manage_gigs',
-			id: 'fivver'
-		}
-	];
+	import Button from '$lib/components/ui/button/button.svelte';
+	import Gmail from '$lib/components/widgets/svgIcons/Gmail.svelte';
+	import Linkedin from '$lib/components/widgets/svgIcons/Linkedin.svelte';
+	import Upwork from '$lib/components/widgets/svgIcons/Upwork.svelte';
+	import Fiveer from '$lib/components/widgets/svgIcons/Fiverr.svelte';
+	import Github from '$lib/components/widgets/svgIcons/Github.svelte';
+	import Fiverr from '$lib/components/widgets/svgIcons/Fiverr.svelte';
 </script>
 
-<main class="relative h-auto w-full overflow-hidden">
-	<section class="grid h-dvh w-full grid-cols-2 grid-rows-2" id="main-container">
+<section class="relative h-auto w-full overflow-hidden">
+	<div class="relative mx-auto flex h-18 w-[95%] items-center justify-center overflow-hidden">
+		<h1 class="text-3xl font-semibold">Talk to me</h1>
+	</div>
+	<div class="grid h-80 w-full grid-cols-3 border-t-[1px] border-b-[1px] dark:border-white border-black">
 		<div
-			class="relative flex h-full w-full flex-col items-center justify-center gap-4 overflow-hidden border-r-[1px] border-gray-600"
-			id="top-left"
+			class="relative col-span-1 flex h-full w-full flex-col items-center justify-center gap-4"
 		>
-			<h1 class="text-3xl font-semibold text-purple-700">Send me an email in my inbox ✍️</h1>
-			<button
+			<p class="text-2xl font-semibold">Write me a mail</p>
+			<Button
 				type="submit"
 				class="rounded-lg px-6 py-3 text-4xl font-semibold shadow"
 				id="gmail-btn"
 			>
-				<a href="mailTo:peakdev698@gmail.com">
-                    <Send />
+				<a href="https://mail.google.com/mail/?view=cm&fs=1&to=peakdev698@gmail.com">
+					<Gmail />
 				</a>
-			</button>
-			<span class="px-4 text-center text-sm text-balance"
-				>I use a public email and dev handle here for privacy. My full name and contact details are
-				available on request or via LinkedIn</span
-			>
+			</Button>
 		</div>
-		<div class="bg relative h-full w-full overflow-hidden" id="top-right"></div>
-		<div
-			class="relative flex h-full w-full flex-col items-center justify-center gap-4 overflow-hidden border-t-[1px] border-r-[1px] border-gray-600"
-			id="bottom-left"
-		>
-			<h1 class="text-3xl font-semibold text-purple-700">Hire me for work or help 😁</h1>
-			<div class="flex items-center justify-center gap-4" id="container-bottom-left">
-				{#each Freelances as freelance}
-					<button type="submit" class="rounded-lg px-6 py-3 text-4xl font-semibold shadow">
-						<a href={freelance.href}>
-							<Linkedin />
-						</a>
-					</button>
-				{/each}
+		<div class="relative col-span-1 flex h-full w-full flex-col items-center justify-center gap-4 border-l-[1px] border-r-[1px] dark:border-white border-black">
+			<p class="text-2xl font-semibold">Check me !!!</p>
+			<div class="flex items-center justify-center gap-6">
+				<a href="https://www.linkedin.com/in/seridi-mohammed-98a70423b/">
+					<Linkedin />
+				</a>
+				<a href="https://github.com/000bachir">
+					<Github />
+				</a>
 			</div>
 		</div>
-		<div
-			class="relative flex h-full w-full flex-col items-center justify-center gap-4 overflow-hidden border-t-[1px] border-gray-600"
-			id="bottom-right"
-		>
-			<h1 class="text-center text-3xl font-semibold text-purple-700">
-				I might not be the best-but I’m reliable, curious, and I make great coffee. ☕😁
-			</h1>
-			<!-- <div class="flex items-center justify-center gap-4" id="container-bottom-right">
-				{#each Works as work}
-					<button type="submit" class="rounded-lg px-6 py-3 text-4xl font-semibold shadow">
-						<a href={work.href}>
-							<Icon name={work.name} color={'white'} />
-						</a>
-					</button>
-				{/each}
-			</div> -->
-		</div>
-	</section>
-</main>
+		<div class="relative flex h-full w-full flex-col items-center justify-center gap-4">
+			<p class="text-2xl font-semibold">Write me a mail</p>
+			<div class="flex items-center justify-center gap-6">
+				<!--upwork-->
+				<a href="https://www.upwork.com/freelancers/~01a03f0469a3b5fcc2">
+					<Upwork />
+				</a>
 
-<style>
-	
-</style>
+				<!--fivver-->
+				<a href="https://fr.fiverr.com/users/bachir_24/manage_gigs">
+					<Fiveer />
+				</a>
+			</div>
+		</div>
+	</div>
+</section>
